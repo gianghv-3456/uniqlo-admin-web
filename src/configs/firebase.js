aede1d2034a942ffb65ca3166476e6f9e5c5c418
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2024-09-08 23:54:03 +0700

    update/firebase: connect to firebase

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAfKsJpREHk14jYCzhk_OFASjJ7rksI4j0",
-    authDomain: "pistore-6b669.firebaseapp.com",
-    projectId: "pistore-6b669",
-    storageBucket: "pistore-6b669.appspot.com",
-    messagingSenderId: "941584457668",
-    appId: "1:941584457668:web:7e5c7415a3231c2c970e91",
-    measurementId: "G-QBKB81Z3MJ"
+    apiKey: "AIzaSyB-CzTpfq2e0_WTZSdU0tXO-CoSyAatbqg",
+    authDomain: "uniqlo-be.firebaseapp.com",
+    projectId: "uniqlo-be",
+    storageBucket: "uniqlo-be.appspot.com",
+    messagingSenderId: "736465995371",
+    appId: "1:736465995371:web:07b150d69a08adebf7932e",
+    measurementId: "G-52L5G75NMT",
 };
 
 const app = initializeApp(firebaseConfig);
